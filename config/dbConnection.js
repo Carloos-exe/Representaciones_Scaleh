d76af4fe2d60e0c383abe3adbec61cdba3b80abc
Carloos-exe <csz117@hotmail.com>
2024-11-26 23:55:19 -0600

    error en db fixed

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,19 +1,16 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
-// Crear el pool de conexiones
-require('dotenv').config();
-
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-    multipleStatements: true,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  multipleStatements: true,
 });
 
-
-// Exporta el pool de conexiones
 module.exports = db;
